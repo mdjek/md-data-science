c15a04dcc30b844527c5f0165e2cf19915f769f2
Evgeniy <mdjek@mail.ru>
2024-11-21 21:12:00 +0300

    system-design [lab4]: refactoring

--- system-design/lab4/mock_loaders/load_pg_mocks.py
+++ system-design/lab4/mock_loaders/load_pg_mocks.py
@@ -1,36 +1,14 @@
 import json
 from init_pg_db import SessionLocal
 from entities import User, Order, Task
-from typing import Callable
 
-def load_pg_user(entity):
-    return User(
-        username=entity["username"],
-        first_name=entity["first_name"],
-        last_name=entity["last_name"],
-        email=entity["email"],
-        password=entity["password"]
-    )
+MODEL = User | Order | Task
 
-def load_pg_order(entity):
-    return Order(
-        name=entity["name"],
-        description=entity["description"],
-        user_id=entity["user_id"],
-    )
-
-def load_pg_task(entity):
-    return Task(
-        name=entity["name"],
-        description=entity["description"],
-        order_id=entity["order_id"],
-    )
-
-def loader(entity, modelEntity: User | Order | Task):
+def loader(entity, modelEntity: MODEL):
     return modelEntity(**entity)
 
 
-def load_pg_table_mock(data: list, modelEntity: User | Order | Task):
+def load_pg_table_mock(data: list, modelEntity: MODEL):
     db = SessionLocal()
 
     for entity in data:
@@ -41,6 +19,7 @@ def load_pg_table_mock(data: list, modelEntity: User | Order | Task):
         db.commit()
 
     db.close()
+
 
 def load_pg_mock_data():
     mock_map = {
