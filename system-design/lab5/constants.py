b95696d20345aa3df9f4d564bcc49faa67b78653
Evgeniy <mdjek@mail.ru>
2024-11-25 19:19:07 +0300

    system-design [lab5]: refactoring

--- system-design/lab5/constants.py
+++ system-design/lab5/constants.py
@@ -1,4 +1,12 @@
+# Auth
+SECRET_KEY = "your-secret-key"
 
+ALGORITHM = "HS256"
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+# Services
 POSTGRESQL_DATABASE_URL = "postgresql://postgres:pass@db/profi_db"
 
 MONGODB_URI = "mongodb://root:rootpasswd@mongo:27017/"
--- system-design/lab5/routers/auth.py
+++ system-design/lab5/routers/auth.py
@@ -9,12 +9,7 @@ from entities import User
 from init_pg_db import get_db
 from sqlalchemy.orm import Session
 from pymongo import MongoClient
-from constants import MONGODB_URI
-
-SECRET_KEY = "your-secret-key"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
+from constants import MONGODB_URI, SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
 
 # Настройка Mongo   
 client = MongoClient(MONGODB_URI, serverSelectionTimeoutMS=1000)
