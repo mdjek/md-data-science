7013ac41f5812c5c7358e063f62304e8f070945e
Evgeniy <mdjek@mail.ru>
2024-11-25 21:38:15 +0300

    system-design [lab5]: refactoring

--- system-design/lab5/constants.py
+++ system-design/lab5/constants.py
@@ -1,14 +1,9 @@
 # Auth
 SECRET_KEY = "your-secret-key"
-
 ALGORITHM = "HS256"
-
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
 
 # Services
 POSTGRESQL_DATABASE_URL = "postgresql://postgres:pass@db/profi_db"
-
 MONGODB_URI = "mongodb://root:rootpasswd@mongo:27017/"
-
 REDIS_URL = "redis://cache:6379/0"
